2a42ec768d8a56ec7cf1c0b34b9826c6d1180eb1
mariageorgeamin <mariageorgeamin@yahoo.com>
2019-03-14 14:55:12 +0200

    added slug package

--- app/Post.php
+++ app/Post.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
+    use Sluggable;
+
     protected $fillable = [
         'title',
         'description',
@@ -16,5 +19,19 @@ class Post extends Model
     {
         // return $this->belongsTo('App\User');
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,6 +10,7 @@
     <tr>
       <th scope="col">Id</th>
       <th scope="col">Title</th>
+      <th scope="col">Slug</th>
       <th scope="col">Description</th>
       <th scope="col">Posted by</th>
       <th scope="col">Created at</th>
@@ -21,6 +22,7 @@
     <tr>
       <th scope="row">{{$post->id}}</th>
       <td>{{$post->title}}</td>
+      <td>{{$post->slug}}</td>
       <td>{{$post->description}}</td>
       <td>{{ isset($post->user) ? $post->user->name : 'Not Found'}}</td>
       <td>{{$post->created_at->format('Y-m-d')}}</td>
